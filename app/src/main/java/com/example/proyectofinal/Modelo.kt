69b9cfa0fe5aae4e41b911f6174794b1fd1ef223
Diego Ms <diego.mtzs00@gmail.com>
2021-04-11 08:28:37 -0500

    Add array of tolerances for model

--- app/src/main/java/com/example/proyectofinal/Modelo.kt
+++ app/src/main/java/com/example/proyectofinal/Modelo.kt
@@ -4,6 +4,9 @@ class Modelo {
     val coloresBandas= arrayOf<String>("#010101", "#551E12", "#FB0000", "#FF8000", "#FFFF00", "#5FE506", "#13ECF1", "#A524E2", "#828282", "#F4F4F4", "#FFCD48", "#CDCDCD")
     //Negro, Cafe, Rojo, Naranja, Amarillo, Verde, Azul claro, Morado/Violeta, Gris, Blanco, amarillo opaco, gris claro
 
+    val tolerancias= arrayOf<String>("#551E12", "#FB0000", "#FFFF00", "#5FE506", "#13ECF1", "#A524E2", "#828282", "#FFCD48", "#CDCDCD")//Implementar cambio de colores en este último boton y el otro string array en el xml
+    //Cafe, Rojo, Amarillo claro, Verde, Azul claro, Morado/Violeta, Gris, Amarillo opaco, gris claro
+
     val primeraBandaValores= arrayOf<Float>(0F, 10F, 20F, 30F, 40F, 50F, 60F, 70F, 80F, 90F)//ohms
     val segundaBandaValores= arrayOf<Float>(0F, 1F, 2F, 3F, 4F, 5F, 6F, 7F, 8F, 9F)//ohms
     //Primeras 2 bandas se suman nada mas mutuamente, para estos valores la secuancia de colores es la misma que la del primer arreglo
